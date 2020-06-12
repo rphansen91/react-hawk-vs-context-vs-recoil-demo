64d480c39b88f0eda49ba1c4a34da483ccd3030c
rphansen91 <rphansen91@gmail.com>
2020-06-11 22:46:48 -0700

    Data hawk

---
+++ build/precache-manifest.aa32b163b27f8bc2e9acd412815a3fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d45dedd05f84b819c38d2c5496c4bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "993d6c3b3f7167c01638",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e44acd1427a0a786778",
+    "url": "/static/js/2.5f9d39ce.chunk.js"
+  },
+  {
+    "revision": "28aa6679dbf6a0a3b709b859699a8f70",
+    "url": "/static/js/2.5f9d39ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993d6c3b3f7167c01638",
+    "url": "/static/js/main.2360c6a4.chunk.js"
+  },
+  {
+    "revision": "ca7c30d51c62ed685f74",
+    "url": "/static/js/runtime-main.ada986ac.js"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,62 @@ import Grid from "@material-ui/core/Grid";
 import { DataHocTodoList } from "./DataHocTodos";
 import { RecoilTodoList } from "./RecoilTodos";
 import { HawkTodoList } from "./HawkTodos";
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot } from "recoil";
 import { Typography } from "@material-ui/core";
 
-const background = 'linear-gradient(rgba(245, 0, 87, 0.4), rgba(63, 81, 181, 0.4))'
+const background =
+  "linear-gradient(rgba(245, 0, 87, 0.8), rgba(63, 81, 181, 0.8))";
 
 function App() {
   return (
     <>
-    <Box width="100%" style={{ padding: '4em 2em', background, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-      <Typography variant="h2" style={{ color: '#fff' }}>Todo List</Typography>
-      <Typography variant="h4" style={{ color: '#fff' }}>Context vs Recoil vs Hawk</Typography>
-    </Box>
-    <Box p={3}>
-      <Grid container spacing={3}>
-        <Grid item md={4}>
-          <DataHocTodoList />
+      <a href="https://github.com/rphansen91/react-hawk" style={{ position: 'absolute', top: 0, right: 0 }}>
+        <img
+          width="149"
+          height="149"
+          src="https://github.blog/wp-content/uploads/2008/12/forkme_right_gray_6d6d6d.png?resize=149%2C149"
+          className="attachment-full size-full"
+          alt="Fork me on GitHub"
+          data-recalc-dims="1"
+        />
+      </a>
+      <Box
+        width="100%"
+        style={{
+          padding: "4em 2em",
+          background,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          boxSizing: 'border-box'
+        }}
+      >
+        <Typography variant="h2" style={{ color: "#fff" }}>
+          Todo List
+        </Typography>
+        <Typography variant="h4" style={{ color: "#fff" }}>
+          Context vs Recoil vs Hawk
+        </Typography>
+      </Box>
+      <Box p={3}>
+        <Grid container spacing={3}>
+          <Grid item md={4}>
+            <DataHocTodoList />
+          </Grid>
+          <Grid item md={4}>
+            <RecoilRoot>
+              <RecoilTodoList />
+            </RecoilRoot>
+          </Grid>
+          <Grid item md={4}>
+            <RecoilRoot>
+              <HawkTodoList />
+            </RecoilRoot>
+          </Grid>
         </Grid>
-        <Grid item md={4}>
-          <RecoilRoot>
-            <RecoilTodoList />
-          </RecoilRoot>
-        </Grid>
-        <Grid item md={4}>
-          <RecoilRoot>
-            <HawkTodoList />
-          </RecoilRoot>
-        </Grid>
-      </Grid>
-    </Box>
-    <Box width="100%" style={{ paddingTop: '20%', background }} />
+      </Box>
+      <Box width="100%" style={{ paddingTop: "20%", background }} />
     </>
   );
 }
