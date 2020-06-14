c34b3f657f2f9f57a8eec46722fae13582b901d3
rphansen91 <rphansen91@gmail.com>
2020-06-14 16:19:40 -0700

    Updates

--- precache-manifest.8e9225740afa7b15253744b70086c4b0.js
+++ precache-manifest.8e9225740afa7b15253744b70086c4b0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e6d362bb85a5da90c922b449c6bc5c94",
+    "revision": "9b898590661f4448c3088e192ae288e2",
     "url": "/react-hawk-vs-context-vs-recoil-demo/index.html"
   },
   {
-    "revision": "bfd9fdbeb4170e1457b4",
+    "revision": "4929c855cd3eb0e85890",
     "url": "/react-hawk-vs-context-vs-recoil-demo/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "18626560985f955ee047",
-    "url": "/react-hawk-vs-context-vs-recoil-demo/static/js/2.d9887b9b.chunk.js"
+    "revision": "dee5fd3becda8fcdf93c",
+    "url": "/react-hawk-vs-context-vs-recoil-demo/static/js/2.a1da9e42.chunk.js"
   },
   {
     "revision": "28aa6679dbf6a0a3b709b859699a8f70",
-    "url": "/react-hawk-vs-context-vs-recoil-demo/static/js/2.d9887b9b.chunk.js.LICENSE.txt"
+    "url": "/react-hawk-vs-context-vs-recoil-demo/static/js/2.a1da9e42.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bfd9fdbeb4170e1457b4",
-    "url": "/react-hawk-vs-context-vs-recoil-demo/static/js/main.ef4a510d.chunk.js"
+    "revision": "4929c855cd3eb0e85890",
+    "url": "/react-hawk-vs-context-vs-recoil-demo/static/js/main.da31b4e0.chunk.js"
   },
   {
     "revision": "5e02032737e9d46fef9f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-hawk-vs-context-vs-recoil-demo/precache-manifest.2b5d27c2141098e0535da25f4a34c58a.js"
+  "/react-hawk-vs-context-vs-recoil-demo/precache-manifest.8e9225740afa7b15253744b70086c4b0.js"
 );
 
 self.addEventListener('message', (event) => {
