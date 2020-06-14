6fc39c7d7a920fe707a97e507f9b8f90aeb392a2
rphansen91 <rphansen91@gmail.com>
2020-06-14 16:19:28 -0700

    include z

--- src/todoUtils.tsx
+++ src/todoUtils.tsx
@@ -28,7 +28,7 @@ export const initialTodoList = alphabet.map((text) => buildTodo(text))
 
 function range (start: number, end: number) {
   const range = []
-  for (let i = start; i < end; i++) {
+  for (let i = start; i <= end; i++) {
     range.push(i)
   }
   return range
